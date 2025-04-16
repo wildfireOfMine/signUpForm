b5325b72b05415877b93732bb0d8b9c8eecfa890
wildfireOfMine <iamtheanguera@gmail.com>
2025-04-16 10:41:46 +0200

    Finished architecture/skeleton of the SPA

--- src/SPA.jsx
+++ src/SPA.jsx
@@ -1,15 +1,38 @@
-import { Drawer } from '@mui/material'
+import { Drawer, List, ListItem, ListItemText } from '@mui/material'
 import React from 'react'
 import SignUp from './components/SignUp'
 
 const SPA = () => {
   return (
     <>
-{/*     <Drawer>
-        {DrawerList}
-    </Drawer> */}
-        <h1>Halo</h1>
+    <Drawer
+        sx={{
+          width: '35%', // 20% del ancho
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: '35%', // 20% del ancho
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent" // Sidebar fijo
+        anchor="left"
+      >
+      </Drawer>
+      <Drawer
+      sx={{
+          width: '65%', // 20% del ancho
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: '65%', // 20% del ancho
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent" // Sidebar fijo
+        anchor="right"
+        >
         <SignUp/>
+      </Drawer>
+
     </>
   )
 }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,17 +1,33 @@
-import { Box, Button, TextField } from '@mui/material'
+import { Box, Button, List, ListItem, ListItemText, TextField } from '@mui/material'
 import React from 'react'
 
 const SignUp = () => {
   return (
     <Box>
         <h1>Welcome!</h1>
-        <TextField id="outlined-basic" label="First name" variant="outlined" />
-        <TextField id="outlined-basic" label="Last name" variant="outlined" />
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-        <TextField id="outlined-basic" label="Phone number" variant="outlined" />
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
-        <TextField id="outlined-basic" label="Confirm password" variant="outlined" />
-        <Button variant="contained">Create account</Button>
+        <List>
+            <ListItem>
+                <TextField id="outlined-basic" label="First name" variant="outlined" />
+            </ListItem>
+            <ListItem>
+                <TextField id="outlined-basic" label="Last name" variant="outlined" />
+            </ListItem>
+            <ListItem>
+                <TextField id="outlined-basic" label="Email" variant="outlined" />
+            </ListItem>
+            <ListItem>
+                <TextField id="outlined-basic" label="Phone number" variant="outlined" />
+            </ListItem>
+            <ListItem>
+                <TextField id="outlined-basic" label="Password" variant="outlined" />
+            </ListItem>
+            <ListItem>
+                <TextField id="outlined-basic" label="Confirm password" variant="outlined" />
+            </ListItem>
+            <Button variant="contained">Create account</Button>
+        </List>
+        <h3>Already have an account? <a href="#">Log in</a></h3>
+        
     </Box>
   )
 }
